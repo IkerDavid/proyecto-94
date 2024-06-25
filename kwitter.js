e038ac4402e75b13d584566b8b994c6923097c0b
IkerDavid <155900230+IkerDavid@users.noreply.github.com>
2024-06-25 13:32:42 -0600

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAngcLBWob8RxMlYHxvj_7XQd5lSjSOzOU",
+    authDomain: "proyecto-94.firebaseapp.com",
+    databaseURL: "https://proyecto-94-default-rtdb.firebaseio.com",
+    projectId: "proyecto-94",
+    storageBucket: "proyecto-94.appspot.com",
+    messagingSenderId: "620541339294",
+    appId: "1:620541339294:web:228d737058d77f824e3d8f",
+    measurementId: "G-XMYBWBQHW8"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
